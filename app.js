14f5321ae556f97485fceba08c949b206546dfdf
AlessandroPuglisi <alessandropuglisi2000@gmail.com>
2024-11-19 14:03:46 +0100

    ADD: foundation creation

---
+++ app.js
@@ -0,0 +1,14 @@
+console.log("TEST")
+
+const express = require('express')
+const app = express()
+const port = 3000
+const postsRouter = require("./routers/posts.js")
+
+app.use(express.static('public'))
+
+app.use("/posts", postsRouter )
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`)
+})
---
+++ routers/posts.js
@@ -0,0 +1,7 @@
+const express = require("express")
+const router = express.Router()
+const posts = require("../posts")
+
+//CRUD 
+
+module.exports = router
